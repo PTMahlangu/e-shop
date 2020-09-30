c76a4e13c6f1e9b157c715207bd63f8ed3a2d101
PTMahlangu <34237978+PTMahlangu@users.noreply.github.com>
2020-09-30 20:55:52 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
     // Database configuration 
-    $Host     = 'localhost'; 
-    $Username = 'root'; 
-    $Password = ''; 
-    $DatabaseName     = 'mbali'; 
+    $Host     = 'sql10.freemysqlhosting.net'; 
+    $Username = 'sql10368085'; 
+    $Password = 'HYvPbh31Fs'; 
+    $DatabaseName     = 'sql10368085'; 
 
     // Create database connection 
     $conn = new mysqli($Host,$Username,$Password,$DatabaseName) or  die("Connection failed: " . $conn->connect_error); 
